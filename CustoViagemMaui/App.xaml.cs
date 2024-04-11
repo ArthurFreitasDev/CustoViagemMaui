d87b4ec26aeef20ed186d19589590e8682c9b6e5
Usuario <Usuario@CPD03-M16>
2024-04-11 15:26:27 -0300

    Continuidade mainpage

--- CustoViagemMaui/App.xaml.cs
+++ CustoViagemMaui/App.xaml.cs
@@ -8,5 +8,19 @@
 
             MainPage = new AppShell();
         }
+
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 460;
+            const int newHeight = 820;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
+        }
     }
 }
